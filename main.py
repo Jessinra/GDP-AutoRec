73670f05c8720c084fb48d9504f430298a12239b
Jessinra <jessinra@gmail.com>
2019-06-10 16:45:08 +0700

    edit epoch save rate

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='I-AutoRec ')
 parser.add_argument('--hidden_neuron', type=int, default=500)
 parser.add_argument('--lambda_value', type=float, default=1)
 
-parser.add_argument('--train_epoch', type=int, default=1000)
+parser.add_argument('--train_epoch', type=int, default=500)
 parser.add_argument('--batch_size', type=int, default=200)
 
 parser.add_argument('--optimizer_method',
@@ -29,7 +29,7 @@ parser.add_argument('--decay_epoch_step', type=int, default=50,
                     help="decay the learning rate for each n epochs")
 
 parser.add_argument('--random_seed', type=int, default=1000)
-parser.add_argument('--display_step', type=int, default=1)
+parser.add_argument('--display_step', type=int, default=5)
 
 args = parser.parse_args()
 print("\n ===> Args parsed\n")
